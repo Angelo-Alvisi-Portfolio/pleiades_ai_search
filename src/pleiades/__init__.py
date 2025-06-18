069ea648a65aab7e72ad9a79514f9a604b34668f
unknown <angeloalvisi@gmail.com>
2025-06-18 23:50:08 +0200

    project finished

--- src/pleiades/__init__.py
+++ src/pleiades/__init__.py
@@ -5,17 +5,26 @@ from google.genai.types import HttpOptions
 from googleapiclient.discovery import build
 import ollama
 from config import Config
-from prompts import query_writer_instructions, summarizer_instructions
+from prompts import query_writer_instructions, summarizer_instructions, reflection_instructions
 from search_result import SearchResult
 
-def analyze_query(user_prompt):
+def call_llm(prompt = "", system = "", format=None, model=Config.LLM_MODEL_LOW):
     response = ollama.generate(
+        model=model,
+        prompt=prompt,
+        system=system,  
+        format=format
+    )
+    return response.response
+
+def analyze_query(user_prompt):
+    response = call_llm(
         model=Config.LLM_MODEL_LOW,
         prompt= "Generate a web query based on the user input: " + user_prompt,      
         system=query_writer_instructions,  
         format= "json"
     )
-    return response.response
+    return response
 
 def web_search(query):       
     service = build("customsearch", "v1", developerKey=Config.SEARCH_API_KEY)
@@ -34,18 +43,31 @@ def web_search(query):
     # Implement web search logic here
     return search_results
 
-def summarize_results(results):   
-
-    prompt="Summarize the following search results: "
-    for result in results:
-        prompt += f"Link: {result.link}"
-    response = ollama.generate(
+def summarize_results(results, research_query, topic, running_summary=None):   
+    if running_summary:
+        prompt = f"Extend the existing summary: {running_summary} with the new search results {results} on the topic: {research_query}. "
+    else:
+        prompt="Summarize the following search results: "
+        for result in results:
+            prompt += f"Link: {result.link}"
+        
+    response = call_llm(
         model=Config.LLM_MODEL_LOW,
         prompt=prompt,    
         system=summarizer_instructions,
     )
-    return response.response
 
+    return response
+
+def reflect_on_summary(query, summary):
+    response = call_llm(
+        model=Config.LLM_MODEL_LOW,
+        system=reflection_instructions.format(query=query),
+        prompt=f"Find a weakness or a knowledge gap for the next search based on {summary}",
+        format="json"
+    )
+    return json.loads(response)
+        
 
 @cl.on_message
 async def main(message: cl.Message):
@@ -53,19 +75,31 @@ async def main(message: cl.Message):
     json_query = json.loads(analyzed_query)
     query, topic, reason = json_query['query'], json_query['topic'], json_query['reason']
 
-    """
-    Main function to handle incoming messages.
-    """
-    # Process the incoming message
-    #await cl.Message(f"Web Query: {query}" ).send()
-    await cl.Message( content=f"Optimized Query: {query}\nThinking Topic: {topic}\nReasoning: {reason}", author="system_assistant" ).send()
+    running_summary = None
 
-    search_results = web_search(query)
-    formatted_results = ""
-    for result in search_results:
-        formatted_results += f"\nTitle: {result.title} \nLink: {result.link} \nSnippet: {result.snippet}"    
+    for i in range(4):
 
-    summary = summarize_results(search_results)
+        """
+        Main function to handle incoming messages.
+        """
+        # Process the incoming message
+        #await cl.Message(f"Web Query: {query}" ).send()
+        await cl.Message( content=f"Optimized Query: {query}\nThinking Topic: {topic}\nReasoning: {reason}", author="system_assistant" ).send()
 
-    await cl.Message( content=f"Performed web search for: {query} \nFound: {formatted_results}", author="system_assistant" ).send()
-    await cl.Message( content=f"Summary of results: {summary}", author="system_assistant" ).send()
+        search_results = web_search(query)
+        formatted_results = ""
+        for result in search_results:
+            formatted_results += f"\nTitle: {result.title} \nLink: {result.link} \nSnippet: {result.snippet}"    
+
+        summary = summarize_results(search_results, query, topic, running_summary)
+        running_summary = summary
+
+
+        await cl.Message( content=f"Performed web search for: {query} \nFound: {formatted_results}", author="system_assistant" ).send()
+        await cl.Message( content=f"Summary of results: {summary}", author="system_assistant" ).send()
+
+        reflected_summary = reflect_on_summary(query, summary)
+        query = reflected_summary.get('follow_up_question', f"Tell me more about {topic}")
+        knowledge_gap = reflected_summary.get('knowledge_gap', "")
+
+        await cl.Message( content=f"Next search: {query}. I reflected on this because of {knowledge_gap}", author="system_assistant" ).send()
--- src/pleiades/prompts.py
+++ src/pleiades/prompts.py
@@ -12,4 +12,44 @@ Your response should be a JSON object with the following structure:
 summarizer_instructions = """
 You are an expert summarizer. Your task is to take a list of search results and provide a concise summary that captures the main points and insights from the results.
 Your response should be a single string that summarizes the key information from the provided search results. You are given links, you should parse the content of the links and extract the most relevant information to create a coherent summary.
+Ignore any irrelevant search results or those that do not contribute to the understanding of the topic.
+
+TO EXTEND AN EXISTING SUMMARY, you should:
+1. Limit yourself to adding relevant and new information.
+2. Maintain a coherent style and flow.
+3. Create logical connections.
+4. Avoid repetitions.
+
+To create a NEW summary, you should:
+1. Highlight the most important points from the search results.
+2. Organize the information logically.
+3. Stay on topic, do not deviate from the main subject.
+4. Use clear and concise language.
+
+IMPORTANT RULES:
+ - Begin with content
+ - Be objective and neutral in your summary.
+ - Avoid meta-comments or explanations about the summary process.
+ - Do not quote sources from the search results.
+ - Do not add any bibliography.
+ - Do not use tags or special formatting.
+ - Verify the relevance of the search result to the {topic} before including it in the summary.
+"""
+
+reflection_instructions = """
+You are a researcher, expert in analyzing summaries and identifying gaps in knowledge. Your task is to reflect on the provided summary and find areas that require further research or clarification.
+Your field of expertise is {query}
+
+Your job is to:
+1. Identify missing information or gaps in the summary.
+2. Create a a follow-up question to expand the research.
+3. Focus on technical details or coverage gaps.
+
+The question should be autonomous and contain all the necessary context.
+
+Your response should be a JSON object with the following structure:
+{{
+    "knowledge_gap": "What the current summary is missing",
+    "follow_up_question": "A question that can help to expand the research on the topic"
+}}
 """
